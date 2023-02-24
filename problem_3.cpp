ed11fdd3c4d6b573f48819f2e4c58c4014b98425
221029cs06 <113498631+221029cs06@users.noreply.github.com>
2023-02-24 21:26:03 +0300

    Create problem_3.cpp

---
+++ problem_3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int it = 6;
+  float A = 4.3;
+   int eng = 8;
+float B = 3.4;
+int math = 2;
+float F = 2.5;
+float c;
+float b;
+float d;
+float e;
+float s;
+c = A * it;
+b = B * eng;
+d = F * math;
+cout<<c<<" "<<b<<" "<<c<<endl;
+e = c+b+d;
+cout<<e<<endl;
+s = it+eng+math;
+cout<<s<<endl;
+cout<<e/s;
+
+    return 0;
+}
